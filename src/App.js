421ebacae9e471dcd4af6a8e586e3cc3169009db
Michael Pieper <michaelepieper@gmail.com>
2019-10-20 01:46:41 -0500

    Update Project component to display data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import Navbar from '../src/components/Navbar/Navbar'
 import './App.scss';
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my website!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Home/>
       </header>
     </div>
   );
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom';
 import './Project.scss';
 
-function Project() {
-    return (
-        <div className="project-frame">
-            <h2>My Project Title</h2>
-            <p>Starting a description of the project...</p>
-        </div>
-    );
+class Project extends React.Component {
+    render() {
+        return (
+            <Link to={`/project/${this.props.id}`} className="link-wrapper">
+                <div className="project-frame">
+                    <h3>{this.props.title}</h3>
+                    <h5>{this.props.time}</h5>
+                    <p>{this.props.description}</p>
+                </div>
+            </Link>
+        );
+    }
 }
+
+Project.propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+};
 
 export default Project;
